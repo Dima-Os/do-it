3d38199631b857dc6fe602d5f47810714449169d
Dima Osipov <osipovdima1989@gmail.com>
2020-10-26 16:34:38 +0200

    added background image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import 'normalize-scss';
+import './styles/styles.scss';
 
+/**<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDsjISvjXo1sWKVq4Fygw7oQgM_OVZOiP0",
+    authDomain: "to-do-5c704.firebaseapp.com",
+    databaseURL: "https://to-do-5c704.firebaseio.com",
+    projectId: "to-do-5c704",
+    storageBucket: "to-do-5c704.appspot.com",
+    messagingSenderId: "235738591854",
+    appId: "1:235738591854:web:66f94a41ccc60d44ec2c7f",
+    measurementId: "G-BV0SRXNVJ9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script> */
+
+/**https://nicepage.com/html-templates/preview/business-services-105595?device=desktop */
